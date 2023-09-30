57cdefbec7c92908d01e9c6ff266494ac1a1df50
rachel2bester <rachel2bester@gmail.com>
2023-09-30 16:37:31 +0100

    sorted fonts and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import React, { useState } from "react";
 import Home from './containers/Home/Home';
 import { Route, Routes, HashRouter as Router } from 'react-router-dom';
