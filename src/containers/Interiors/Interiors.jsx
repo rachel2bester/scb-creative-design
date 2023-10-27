a92cbb70ac623b61dd2663e31f6b5fcfac345349
rachel2bester <rachel2bester@gmail.com>
2023-10-27 21:15:38 +0100

    fixed interiors button

--- src/containers/Interiors/Interiors.jsx
+++ src/containers/Interiors/Interiors.jsx
@@ -100,19 +100,9 @@ const Interiors = (props) => {
             />
 
             <div className={sideBarButtonClassName} onClick={toggleDisplaySideBar}>           
-                {displaySideBar ?
-                    <svg 
-                        viewBox="0 0 24 24" 
-                        transform="scale (-1, 1)" 
-                        transform-origin="center"
-                    >
-                        <path d="M4,2H2V22H4V13H18.17L12.67,18.5L14.08,19.92L22,12L14.08,4.08L12.67,5.5L18.17,11H4V2Z" />
-                    </svg> 
-                    :
-                    <svg viewBox="0 0 24 24">
-                        <path d="M4,2H2V22H4V13H18.17L12.67,18.5L14.08,19.92L22,12L14.08,4.08L12.67,5.5L18.17,11H4V2Z" />
-                    </svg>
-                }
+                <svg viewBox="0 0 24 24">
+                    <path d="M4,2H2V22H4V13H18.17L12.67,18.5L14.08,19.92L22,12L14.08,4.08L12.67,5.5L18.17,11H4V2Z" />
+                </svg> 
             </div>
         
             <div className="interiors__content">
