47c7e3e4a6c238c958e0fdf407af3b620ee9d62e
rachel2bester <rachel2bester@gmail.com>
2025-05-27 18:24:15 +0100

    Added more projects

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -15,7 +15,6 @@ import Earlswood13 from "./../images/Earlswood/13.jpeg"
 import Earlswood14 from "./../images/Earlswood/14.jpeg"
 import Earlswood15 from "./../images/Earlswood/15.jpeg"
 
-
 import Khyalanga1 from "./../images/Khyalanga/1.jpg"
 import Khyalanga2 from "./../images/Khyalanga/2.JPG"
 import Khyalanga3 from "./../images/Khyalanga/3.jpg"
@@ -39,13 +38,66 @@ import Khyalanga20 from "./../images/Khyalanga/20.jpg"
 import Khyalanga21 from "./../images/Khyalanga/21.jpg"
 import Khyalanga22 from "./../images/Khyalanga/22.jpg"
 import Khyalanga23 from "./../images/Khyalanga/23.jpg"
-import Khyalanga24 from "./../images/Khyalanga/24.jpg"
-import Khyalanga25 from "./../images/Khyalanga/25.jpg"
-import Khyalanga26 from "./../images/Khyalanga/26.jpg"
-import Khyalanga27 from "./../images/Khyalanga/27.jpg"
-import Khyalanga28 from "./../images/Khyalanga/28.jpg"
 
+import Hurlingham1 from "./../images/Hurlingham/1.jpg"
+import Hurlingham2 from "./../images/Hurlingham/2.jpeg"
+import Hurlingham3 from "./../images/Hurlingham/3.jpg"
+import Hurlingham4 from "./../images/Hurlingham/4.jpg"
+import Hurlingham5 from "./../images/Hurlingham/5.jpg"
+import Hurlingham6 from "./../images/Hurlingham/6.jpg"
+import Hurlingham7 from "./../images/Hurlingham/7.jpg"
+import Hurlingham8 from "./../images/Hurlingham/8.jpeg"
+import Hurlingham9 from "./../images/Hurlingham/9.jpeg"
+import Hurlingham10 from "./../images/Hurlingham/10.jpg"
+import Hurlingham11 from "./../images/Hurlingham/11.jpeg"
+import Hurlingham12 from "./../images/Hurlingham/12.jpg"
+import Hurlingham13 from "./../images/Hurlingham/13.jpg"
+import Hurlingham14 from "./../images/Hurlingham/14.jpg"
+import Hurlingham15 from "./../images/Hurlingham/15.jpeg"
+import Hurlingham16 from "./../images/Hurlingham/16.png"
+import Hurlingham17 from "./../images/Hurlingham/17.jpg"
+import Hurlingham18 from "./../images/Hurlingham/18.jpg"
+import Hurlingham19 from "./../images/Hurlingham/19.jpg"
+import Hurlingham20 from "./../images/Hurlingham/20.jpg"
 
+import Oxshott1 from "./../images/Oxshott/1.jpeg"
+import Oxshott2 from "./../images/Oxshott/2.jpeg"
+import Oxshott3 from "./../images/Oxshott/3.jpg"
+import Oxshott4 from "./../images/Oxshott/4.jpeg"
+import Oxshott5 from "./../images/Oxshott/5.jpg"
+import Oxshott6 from "./../images/Oxshott/6.jpeg"
+import Oxshott7 from "./../images/Oxshott/7.jpeg"
+import Oxshott8 from "./../images/Oxshott/8.jpg"
+import Oxshott9 from "./../images/Oxshott/9.jpeg"
+import Oxshott10 from "./../images/Oxshott/10.jpg"
+import Oxshott11 from "./../images/Oxshott/11.jpeg"
+import Oxshott12 from "./../images/Oxshott/12.jpeg"
+
+import ElementBay1 from "./../images/ElementBay/1.jpg"
+import ElementBay2 from "./../images/ElementBay/2.jpg"
+import ElementBay3 from "./../images/ElementBay/3.jpg"
+import ElementBay4 from "./../images/ElementBay/4.jpg"
+import ElementBay5 from "./../images/ElementBay/5.jpg"
+import ElementBay6 from "./../images/ElementBay/6.jpg"
+import ElementBay7 from "./../images/ElementBay/7.jpg"
+import ElementBay8 from "./../images/ElementBay/8.jpg"
+
+import Mozambique1 from "./../images/Mozambique/1.jpeg"
+import Mozambique2 from "./../images/Mozambique/2.jpeg"
+import Mozambique3 from "./../images/Mozambique/3.jpeg"
+import Mozambique4 from "./../images/Mozambique/4.jpeg"
+import Mozambique5 from "./../images/Mozambique/5.jpeg"
+import Mozambique6 from "./../images/Mozambique/6.jpeg"
+
+import Sparta1 from "./../images/Sparta/1.jpg"
+import Sparta2 from "./../images/Sparta/2.jpg"
+import Sparta3 from "./../images/Sparta/3.jpg"
+import Sparta4 from "./../images/Sparta/4.jpg"
+import Sparta5 from "./../images/Sparta/5.jpg"
+import Sparta6 from "./../images/Sparta/6.jpg"
+import Sparta7 from "./../images/Sparta/7.jpg"
+import Sparta8 from "./../images/Sparta/8.jpg"
+import Sparta9 from "./../images/Sparta/9.jpg"
 
 const projects = [
     {
@@ -100,7 +152,7 @@ const projects = [
             {
                 imgSrc: Earlswood10,
                 alt: "Bedroom",
-                displayOnMain: true
+                displayOnMain: false
             },
             {
                 imgSrc: Earlswood11,
@@ -129,61 +181,59 @@ const projects = [
             }
         ]
     },
-
-
     {
-        projectName: "Umhlanga, South Africa Beach Apartment: Pristine, Meticulous, Tasteful",
+        projectName: "Umhlanga, South Africa Beach Apartment - Pristine, Meticulous, Tasteful",
         projectBrief: "",
         images: [
             {
                 imgSrc: Khyalanga1,
-                alt: "Dining Area",
-                displayOnMain: false
-            },
-            {
-                imgSrc: Khyalanga2,
-                alt: "Entry hall",
+                alt: "Living Area",
                 displayOnMain: true
             },
             {
-                imgSrc: Khyalanga3,
-                alt: "Dining Area",
+                imgSrc: Khyalanga2,
+                alt: "Living Area",
                 displayOnMain: false
             },
             {
+                imgSrc: Khyalanga3,
+                alt: "Living Area",
+                displayOnMain: true
+            },
+            {
                 imgSrc: Khyalanga4,
-                alt: "Dining Area",
+                alt: "Living Area",
                 displayOnMain: false
             },
             {
                 imgSrc: Khyalanga5,
-                alt: "Bathroom",
-                displayOnMain: true
+                alt: "Living Area",
+                displayOnMain: false
             },
             {
                 imgSrc: Khyalanga6,
-                alt: "Bedroom",
-                displayOnMain: true
+                alt: "Living Area",
+                displayOnMain: false
             },
             {
                 imgSrc: Khyalanga7,
-                alt: "Living room",
-                displayOnMain: true
+                alt: "TV Nook",
+                displayOnMain: false
             },
             {
                 imgSrc: Khyalanga8,
-                alt: "Living room",
+                alt: "TV Nook",
                 displayOnMain: false
             },
             {
                 imgSrc: Khyalanga9,
-                alt: "Child's bedroom",
-                displayOnMain: true
+                alt: "Kitchen Dining Area",
+                displayOnMain: false
             },
             {
                 imgSrc: Khyalanga10,
                 alt: "Bedroom",
-                displayOnMain: true
+                displayOnMain: false
             },
             {
                 imgSrc: Khyalanga11,
@@ -192,23 +242,368 @@ const projects = [
             },
             {
                 imgSrc: Khyalanga12,
-                alt: "Entry hall",
-                displayOnMain: false
-            },
-            {
-                imgSrc: Khyalanga13,
-                alt: "Living room",
+                alt: "Bedroom",
                 displayOnMain: true
             },
             {
+                imgSrc: Khyalanga13,
+                alt: "Bedroom",
+                displayOnMain: false
+            },
+            {
                 imgSrc: Khyalanga14,
-                alt: "Living room",
+                alt: "Bathroom",
                 displayOnMain: false
             },
             {
                 imgSrc: Khyalanga15,
+                alt: "Dining Room",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Khyalanga16,
+                alt: "Kitchen with Breakfast bar",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Khyalanga17,
+                alt: "Kitchen with Breakfast bar",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Khyalanga18,
+                alt: "Bedroom",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Khyalanga19,
+                alt: "Kitchen with Breakfast bar",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Khyalanga20,
+                alt: "Breakfast Bar",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Khyalanga21,
+                alt: "Kitchen",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Khyalanga22,
+                alt: "Bathroom",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Khyalanga23,
                 alt: "Living room",
                 displayOnMain: false
+            }
+        ]
+    },
+    {
+        projectName: "Crown Estate Masterpiece - Abundant, Luxurious, Comfortable",
+        projectBrief: "Transforming this exquisite 7-bedroom house into an abundant, yet comfortable family home. We collaborated closely with our client to design bespoke furniture, craft elegant joinery, and add our key signature touches throughout from consulting on the framing and placement of a sophisticated art collection to sourcing the perfect silk rugs that seamlessly complement the client's existing furniture, every detail was carefully curated to achieve a truly luxurious home!",
+        images: [
+            {
+                imgSrc: Hurlingham1,
+                alt: "Dining room",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham2,
+                alt: "Living room",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Hurlingham3,
+                alt: "Living room",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham4,
+                alt: "Entrance hall",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham5,
+                alt: "Study",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Hurlingham6,
+                alt: "Cinema room",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham7,
+                alt: "Study",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham8,
+                alt: "Bathroom",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Hurlingham9,
+                alt: "Bathroom",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Hurlingham10,
+                alt: "Dressing room",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Hurlingham11,
+                alt: "Pool",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham12,
+                alt: "Bedroom",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham13,
+                alt: "Sitting room",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham14,
+                alt: "Bathroom",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham15,
+                alt: "Bar area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Hurlingham16,
+                alt: "Entrance hall sketch",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Hurlingham17,
+                alt: "Sitting Room",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham18,
+                alt: "Display",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Hurlingham19,
+                alt: "Stairs",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Hurlingham20,
+                alt: "Kitchen",
+                displayOnMain: true
+            }
+        ]
+    },
+    {
+        projectName: "Oxshott Home - Seamless, Stylish, Extraordinary",
+        projectBrief: "",
+        images: [
+            {
+                imgSrc: Oxshott1,
+                alt: "Living Area",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Oxshott2,
+                alt: "Living Area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Oxshott3,
+                alt: "Living Area",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Oxshott4,
+                alt: "Living Area",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Oxshott5,
+                alt: "Living Area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Oxshott6,
+                alt: "Dining Area",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Oxshott7,
+                alt: "Dining Area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Oxshott8,
+                alt: "Display",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Oxshott9,
+                alt: "Living Area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Oxshott10,
+                alt: "Display",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Oxshott11,
+                alt: "Living Area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Oxshott12,
+                alt: "Display",
+                displayOnMain: true
+            }
+        ]
+    },
+    {
+        projectName: "Element Bay, Mauritius, Apartment - Polished, Tailored, Inviting",
+        projectBrief: "",
+        images: [
+            {
+                imgSrc: ElementBay1,
+                alt: "Outdoor Dining and Sitting Area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: ElementBay2,
+                alt: "Outdoor Dining and Sitting Area",
+                displayOnMain: true
+            },
+            {
+                imgSrc: ElementBay3,
+                alt: "Outdoor Sitting Area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: ElementBay4,
+                alt: "Outdoor Dining and Sitting Area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: ElementBay5,
+                alt: "Bedroom",
+                displayOnMain: true
+            },
+            {
+                imgSrc: ElementBay6,
+                alt: "Bedroom",
+                displayOnMain: false
+            },
+            {
+                imgSrc: ElementBay7,
+                alt: "Twin Bedroom",
+                displayOnMain: false
+            },
+            {
+                imgSrc: ElementBay8,
+                alt: "Living Room",
+                displayOnMain: true
+            }
+        ]
+    },
+    {
+        projectName: "Vilankulos Mozambique, Beach House - Spontaneous, Simplified, Relaxed",
+        projectBrief: "",
+        images: [
+            {
+                imgSrc: Mozambique1,
+                alt: "Display",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Mozambique2,
+                alt: "Sitting Area",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Mozambique3,
+                alt: "Display",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Mozambique4,
+                alt: "Outdoor Seating",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Mozambique5,
+                alt: "Display",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Mozambique6,
+                alt: "Sitting Area",
+                displayOnMain: false
+            }
+        ]
+    },
+    {
+        projectName: "Sparta Multiuse Offices - Distinct, Efficient and Ergonomic",
+        projectBrief: "",
+        images: [
+            {
+                imgSrc: Sparta1,
+                alt: "Office Entrance",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Sparta2,
+                alt: "Display",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Sparta3,
+                alt: "Office sitting space",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Sparta4,
+                alt: "Conference room",
+                displayOnMain: true
+            },
+            {
+                imgSrc: Sparta5,
+                alt: "Hall",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Sparta6,
+                alt: "Office Kitchen",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Sparta7,
+                alt: "Office space",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Sparta8,
+                alt: "Dining Area",
+                displayOnMain: false
+            },
+            {
+                imgSrc: Sparta9,
+                alt: "Office Kitchen",
+                displayOnMain: true
             }
         ]
     }
